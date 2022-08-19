a37a17bc6d9d2af664602fc177c0365675c2d7fc
Ibrvheem <i.aliyu019@gmail.com>
2022-08-19 01:49:14 +0100

    Navbar commit

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ function displayHelp(){
         help.style.display = "none"
     }
 }
+let solutions = document.getElementById("solutions")
+function displaySolution(){
+    console.log("working")
+    if(solutions.style.display === "none"){
+        solutions.style.display = "flex"
+    }else if(solutions.style.display === "flex"){
+        solutions.style.display = "none"
+    }else{
+        solutions.style.display = "none"
+    }
+}
+let support = document.getElementById("support")
+function displaySupport(){
+    console.log("working")
+    if(support.style.display === "none"){
+        support.style.display = "flex"
+    }else if(support.style.display === "flex"){
+        support.style.display = "none"
+    }else{
+        support.style.display = "none"
+    }
+}
+let about = document.getElementById("about")
+function displayAbout(){
+    console.log("working")
+    if(about.style.display === "none"){
+        about.style.display = "flex"
+    }else if(about.style.display === "flex"){
+        about.style.display = "none"
+    }else{
+        about.style.display = "none"
+    }
+}
